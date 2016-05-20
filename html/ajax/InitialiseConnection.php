130607d27101b7dfff3f70cbbc07ecd6c471dc16
razvanalex <razvansmadu@gmail.com>
2016-05-20 20:24:20 +0300

    fix InitConn

--- html/ajax/InitialiseConnection.php
+++ html/ajax/InitialiseConnection.php
@@ -1,5 +1,14 @@
 <?php
-
-echo 'ghfghgfh';
-	
+    $servername = "localhost";
+    $Susername = "razvanalex";
+    $Spassword = "";
+    $dbname = "Accounts";
+    
+    // Create connection
+    $conn = new mysqli($servername, $Susername, $Spassword, $dbname);
+    // Check connection
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }	
 ?>
